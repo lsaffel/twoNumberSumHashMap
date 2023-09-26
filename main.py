47bd7910c16654a4769ea9e937c1f5773bc12b4b
Lisa <llvvss@icloud.com>
2023-09-26 17:18:28 -0600

    adding a print to show progress of hash map set as program runs

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ def twoSum(nums, target: int):
         if diff in prevMap:
             return [prevMap[diff], i]
         prevMap[n] = i
+        print(prevMap)      # just to show how this program works. Not a necessary line
     return []              # this line is not really necessary, assuming that exactly one answer exists in the list
 
 
 if __name__ == '__main__':
     print(twoSum([1,3], 4))
+    print("///////////////////////////////////////////")
     print(twoSum([1,3,5,7], 12))
+    print("///////////////////////////////////////////")
     print(twoSum([1,3,12,-6], 6))
+    print("///////////////////////////////////////////")
     print(twoSum([5,10,20], 1111))
+    print("///////////////////////////////////////////")
     print(twoSum([5,10,20], 15))
 
